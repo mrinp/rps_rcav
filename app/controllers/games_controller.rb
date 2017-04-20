c77c1845d2a9e022dd1c4faf3859df54f0e11368
mrinp <mrinp@users.noreply.github.com>
2017-04-20 14:02:00 -0500

    Change to speed up loading
    Deleted the following code in the application.js file:
    
    //= require bootstrap-sprockets
    
    This line makes you lose carousels and animated stuff etc. Can be added
    back in if you need to later?

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,9 @@
+class GamesController < ApplicationController
+
+  def play_rock
+
+  render ("games/play_rock.html.erb")  #erb = embedded ruby code
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get("/rock", :controller => "games", :action => "play_rock")
+  #Cannot have a space b/w the get method and it's arguments
+
 end
